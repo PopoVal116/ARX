d5f0d5050f23e95b3f4af81473bb51617b2e1258
annkits <annpodolskaya0965@gmail.com>
2026-03-31 16:24:18 +0700

    дерево по паре автор-жанр

--- c_core/src/sort.c
+++ c_core/src/sort.c
@@ -67,3 +67,30 @@ void sort_songs_by_title_desc(Song *songs, int count, Song **ptrs)
 
     quick_sort_by_title_desc(ptrs, 0, count - 1);
 }
+
+int compare_author_genre(const Song *a, const Song *b) {
+    int res = strcmp(a->author, b->author);
+    if (res != 0) return res;
+    return strcmp(a->genre, b->genre);
+}
+
+int partition_author_genre_asc(Song **arr, int low, int high) {
+    Song *pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (compare_author_genre(arr[j], pivot) <= 0) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return i + 1;
+}
+
+void quick_sort_by_author_genre_asc(Song **arr, int low, int high) {
+    if (low < high) {
+        int pi = partition_author_genre_asc(arr, low, high);
+        quick_sort_by_author_genre_asc(arr, low, pi - 1);
+        quick_sort_by_author_genre_asc(arr, pi + 1, high);
+    }
+}
